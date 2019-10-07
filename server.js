25ad51977345997bc5a984372ab0fed20c9630a2
Keith MacKinnon <keith.mackinnon@ge.com>
2019-10-06 18:37:48 -0700

    Add response header for content source

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const path = require("path");
 const redisClient = require('./lib/redis-client');
 const cache = require('./lib/cache');
 
-// expects stored values to be objects
+// Note: implementation expects stored values to be objects
 app.get('/api/v1/values/:key', async (req, res) => {
   const { key } = req.params;
 
   // check if local cache contains value of requested key
   const val = cache.get(key);
   if (typeof val !== 'undefined') {
+    res.set('Content-Source', 'local-cache');
     return res.json(JSON.parse(val));
   }
 
@@ -23,7 +24,10 @@ app.get('/api/v1/values/:key', async (req, res) => {
     return;
   }
 
+  // store in local cache for future requests
   cache.set(key, redisVal);
+
+  res.set('Content-Source', 'backing-redis');
   return res.json(JSON.parse(redisVal));
 });
 
--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ const PROXY_URL = process.env.PROXY_URL || 'http://localhost:3000';
 
 describe('Redis Proxy', () => {
 
-  it('default route should show static page', (done) => {
+  it('should show static page when default route requested', (done) => {
     chai.request(PROXY_URL)
       .get('/')
       .then((res) => {
@@ -21,7 +21,7 @@ describe('Redis Proxy', () => {
       });
   });
 
-  it('request should fail when invalid api route entered', (done) => {
+  it('should fail when invalid api route entered', (done) => {
     chai.request(PROXY_URL)
       .get('/api/v1/my-key')
       .then((res) => {
@@ -31,19 +31,8 @@ describe('Redis Proxy', () => {
       });
   });
 
-  it('request should fail when key does not exist', (done) => {
-    chai.request(PROXY_URL)
-      .get('/api/v1/values/my-key')
-      .then((res) => {
-        expect(res.text).to.contain('Not Found');
-        expect(res).to.have.status(404);
-        done();
-      });
-  });
-
   describe('with Redis', () => {
     let testVal;
-
     before(async (done) => {
       testVal = {
         'location': 'Vancouver'
@@ -52,16 +41,42 @@ describe('Redis Proxy', () => {
       done();
     });
 
-    it('request should return value in cache', (done) => {
+    function verifyContentSource(source, cb) {
       chai.request(PROXY_URL)
         .get('/api/v1/values/test-key')
         .then((res) => {
           expect(res.text).to.contain(JSON.stringify(testVal));
+          expect(res).to.have.header('Content-Source', source);
           expect(res).to.have.status(200);
-          done();
+          cb();
         }).catch((err) => {
-          done(err);
+          cb(err);
+        });
+    }
+
+    it('should fail when key does not exist', (done) => {
+      chai.request(PROXY_URL)
+        .get('/api/v1/values/my-key')
+        .then((res) => {
+          expect(res.text).to.contain('Not Found');
+          expect(res).to.have.status(404);
+          done();
         });
     });
+
+    it('should return value from backing redis', (done) => {
+      verifyContentSource('backing-redis', done);
+    });
+
+    it('should return value from local cache on subsequent call', (done) => {
+      verifyContentSource('local-cache', done);
+    });
+
+    // TODO max keys locally
+
+    // TODO LRU eviction
+
+    // TODO cache expiry
+
   });
 });
